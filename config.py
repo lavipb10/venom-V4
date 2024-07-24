77ab16845deee145783d3139603c8c649d4293e3
lavipb10 <love5911love@gmail.com>
2024-07-25 03:42:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7200103971:AAEq7qyNsq-n1GtBJKza76agSXICScL7pdM")
+BOT_TOKEN=("7499509205:AAHZKYjIEf3gsKVfM3CydtGYw3ZS-LUm4XA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("loveXddos")
+OWNER_USERNAME=("crazyXvenom")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5808806086")
+ADMIN_IDS=("7179596738")
 
 
 
